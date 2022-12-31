9d37a134355e5f4589e14958f8ad1cf11e3e1c20
Ashish Nath <ashishnathofficial98@gmail.com>
2022-12-31 13:51:46 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+import "./styles.css";
+
+export default function App() {
+  const [data, setData] = useState("");
+  const [list, setList] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (data === "") return;
+    setList((oldData) => [...oldData, data]);
+    setData("");
+  };
+
+  const handleDelete = (id) => {
+    setList((oldData) => oldData.filter((_elem, index) => index !== id));
+  };
+
+  const handleData = function (e) {
+    setData(e.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <label>
+          Enter Task:
+          <input type="text" value={data} onChange={(e) => handleData(e)} />
+          <input type="submit" value="Submit" />
+        </label>
+      </form>
+      <ol>
+        {list.map((item, id) => {
+          return (
+            <li key={id}>
+              <b>{item} </b>
+              <button onClick={() => handleDelete(id)}>X</button>
+            </li>
+          );
+        })}
+      </ol>
     </div>
   );
 }
-
-export default App;
